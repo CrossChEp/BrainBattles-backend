6db1266a09684ac4547c30b9ede67363ab2dc9f8
Andy <anderzanovoleg@gmail.com>
2022-02-18 22:02:28 +0300

    structure has done

--- store/__init__.py
+++ store/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from db_model import User
+from store.db_model import User
 
 
 database_protocol = 'sqlite:///store/database.db'
--- store/db_model.py
+++ store/db_model.py
@@ -1,13 +1,12 @@
-from tokenize import String
 
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 base = declarative_base()
 
 
 class User(base):
-    __tablenam__ = 'users'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     nickname = Column(String)
     email = Column(String)
