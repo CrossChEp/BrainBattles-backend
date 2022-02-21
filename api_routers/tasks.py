d1eebe669e5f4f2f76d239ce57672b9fcca24e91
Andy <anderzanovoleg@gmail.com>
2022-02-21 20:33:00 +0300

    tasks deletion has done

--- api_routers/tasks.py
+++ api_routers/tasks.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from api_routers.auth import get_current_user
 from schemas import TaskModel
 from store import get_session, User
-from models import task_add, tasks_get, task_get
+from models import task_add, tasks_get, task_get, task_delete
 
 tasks_router = APIRouter()
 
@@ -22,3 +22,9 @@ def get_tasks(session: Session = Depends(get_session)):
 @tasks_router.get('/api/task')
 def get_task(task_id: int, session: Session = Depends(get_session)):
     return task_get(task_id=task_id, session=session)
+
+
+@tasks_router.delete('/api/task')
+def delete_task(task_id: int, user: User = Depends(get_current_user),
+                session: Session = Depends(get_session)):
+    return task_delete(task_id=task_id, user=user, session=session)
--- models/tasks_methods.py
+++ models/tasks_methods.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from schemas import TaskModel
@@ -24,3 +25,11 @@ def tasks_get(session: Session):
 def task_get(task_id: int, session: Session):
     task = session.query(Task).filter_by(id=task_id).first()
     return task
+
+
+def task_delete(task_id: int, user: User, session: Session):
+    task = session.query(Task).filter_by(id=task_id).first()
+    if task not in user.tasks:
+        raise HTTPException(status_code=405)
+    session.delete(task)
+    session.commit()
