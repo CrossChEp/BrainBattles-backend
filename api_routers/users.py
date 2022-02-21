97aae0699836cc2da4ff656e03c4f9925ab21a5f
Andy <anderzanovoleg@gmail.com>
2022-02-21 15:59:40 +0300

    handlers renaming has done

--- api_routers/users.py
+++ api_routers/users.py
@@ -9,22 +9,22 @@ from store import users_get, get_session, user_add, user_delete, user_update, Us
 users_router = APIRouter()
 
 
-@users_router.get('/users')
+@users_router.get('/api/users')
 def get_users(session: Session = Depends(get_session)):
     return users_get(session=session)
 
 
-@users_router.post('/register')
+@users_router.post('/api/register')
 def add_user(user: UserModel, session: Session = Depends(get_session)):
     return user_add(user=user, session=session)
 
 
-@users_router.delete('/users')
+@users_router.delete('/api/users')
 def delete_user(id: int, session: Session = Depends(get_session)):
     return user_delete(id=id, session=session)
 
 
-@users_router.put('/users')
+@users_router.put('/api/users')
 def update_user(update_data: UserUpdate, user: User = Depends(get_current_user),
                 session: Session = Depends(get_session)):
     return user_update(user=user, session=session, update_data=update_data)
