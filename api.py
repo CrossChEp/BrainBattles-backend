4735932560fe68a70cad39ced0fe60a0375dd90b
Andy <anderzanovoleg@gmail.com>
2022-02-22 16:13:48 +0300

    bug fixes

--- api.py
+++ api.py
@@ -31,5 +31,6 @@ app.include_router(tasks_router)
 app.include_router(matchmaking_router)
 app.include_router(game_router)
 
+
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
