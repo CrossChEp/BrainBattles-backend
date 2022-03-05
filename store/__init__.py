2043dacab58242d324b5cedabf5d8f11b1055059
Andy <anderzanovoleg@gmail.com>
2022-03-05 16:15:29 +0300

    store docs

--- store/__init__.py
+++ store/__init__.py
@@ -1,3 +1,21 @@
+""" Module for database
+
+    Classes:
+        User
+            users' table
+        Task
+            tasks' table
+        Staging
+            queue's table
+        Game
+            games' table
+
+    Functions:
+        get_session
+            gets database session
+"""
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from store.db_model import User, Task, Staging, Game
