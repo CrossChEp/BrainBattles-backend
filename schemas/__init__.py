aafb7eb401e9cd0aab7c85b29f2500730110c463
Andy <anderzanovoleg@gmail.com>
2022-03-05 14:40:48 +0300

    schemas docs

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,1 +1,15 @@
+"""
+Module of api's schemas
+
+Classes
+    UserModel
+        scheme of user
+    Token
+        scheme of token
+    TokenData
+        scheme of token with username
+    TaskModel
+        scheme of task
+"""
+
 from schemas.api_models import UserModel, Token, TokenData, TaskModel
--- schemas/api_models.py
+++ schemas/api_models.py
@@ -4,6 +4,20 @@ from pydantic import BaseModel
 
 
 class UserModel(BaseModel):
+    """ Scheme of user
+
+        fields:
+        nickname: str
+            user's nickname
+        email: str
+            user's email
+        name: str
+            user's name
+        surname: str
+            user's surname
+        password: str
+            user's password
+    """
     nickname: Optional[str]
     email: Optional[str]
     name: Optional[str]
@@ -21,6 +35,14 @@ class UserUpdate(UserModel):
 
 
 class Token(BaseModel):
+    """ Scheme of token
+
+        fields:
+        access_token: str
+            jwt token
+        token_type: str
+            type of token
+    """
     access_token: str
     token_type: str
 
@@ -29,6 +51,10 @@ class Token(BaseModel):
 
 
 class TokenData(BaseModel):
+    """ Scheme of token with user's username
+        fields:
+        username: Optional[str]
+    """
     username: Optional[str] = None
 
     class Config:
@@ -36,6 +62,19 @@ class TokenData(BaseModel):
 
 
 class TaskModel(BaseModel):
+    """Scheme of task
+        fields:
+        name: str
+            task name
+        subject: str
+            task's subject
+        content: str
+            task's content
+        right_answer: str
+            task's right answer
+        scores: float
+            max scores for this task
+    """
     name: str
     subject: str
     content: str
