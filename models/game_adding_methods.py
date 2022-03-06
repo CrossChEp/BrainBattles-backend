a066c5fb4f6b296da64d9661c055c1f00b0c9567
Andy <anderzanovoleg@gmail.com>
2022-03-06 22:14:02 +0300

    structure refactoring

---
+++ models/game_adding_methods.py
@@ -0,0 +1,76 @@
+from sqlalchemy.orm import Session
+
+from models import delete_from_staging
+from store import Staging, User, Game, Task
+import random
+
+
+def add_ranks_list(ranks: dict) -> list:
+    """
+    creates list from ranks' dict
+    :param ranks: dict
+    :return: list
+    """
+    ranks_list = []
+    for key, value in ranks.items():
+        ranks_list.append(value)
+    return ranks_list
+
+
+def filtered_users(subject: str, session: Session):
+    queue = session.query(Staging).all()
+    filtered = []
+    for user in queue:
+        if user.subject == subject:
+            filtered.append(user)
+    return filtered
+
+
+def get_random_user(users: list):
+    random_id = random.randint(0, len(users))
+    try:
+        random_user = users[random_id]
+        return random_user
+    except IndexError:
+        return False
+
+
+def get_random_task(tasks: list):
+    random_task_index = random.randint(0, len(tasks) - 1)
+    try:
+        random_task = tasks[random_task_index]
+        return random_task
+    except IndexError:
+        return False
+
+
+def search_opponent(users: list, user: User):
+    flag = False
+    random_user = None
+    while not flag:
+        random_user = get_random_user(users=users)
+        if random_user and random_user.user_id != user.id:
+            flag = True
+    return random_user
+
+
+def database_users_adding(user: User, opponent: User, session: Session):
+    user_opponent = Game(opponent_id=opponent.id)
+    opponent_opponent = Game(opponent_id=user.id)
+    session.add(user_opponent)
+    user.game.append(user_opponent)
+    session.add(opponent_opponent)
+    opponent.game.append(opponent_opponent)
+    session.commit()
+    delete_from_staging(user=user, session=session)
+    delete_from_staging(user=opponent, session=session)
+    session.commit()
+
+
+def database_task_adding(task: Task, user_id: int,
+                         opponent_id: int, session: Session):
+    user_game = session.query(Game).filter_by(user_id=user_id, opponent_id=opponent_id).first()
+    opponent_game = session.query(Game).filter_by(user_id=opponent_id, opponent_id=user_id).first()
+    task.games.append(user_game)
+    task.games.append(opponent_game)
+    session.commit()
--- models/game_methods.py
+++ models/game_methods.py
@@ -1,83 +1,10 @@
-import random
-
 from fastapi import HTTPException
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from configs import ranks
-from models import delete_from_staging
+from models.game_adding_methods import filtered_users, search_opponent, get_random_task, database_users_adding, \
+    database_task_adding
 from store import User, Staging, Game, Task
-
-
-def add_ranks_list(ranks: dict) -> list:
-    """
-    creates list from ranks' dict
-    :param ranks: dict
-    :return: list
-    """
-    ranks_list = []
-    for key, value in ranks.items():
-        ranks_list.append(value)
-    return ranks_list
-
-
-def filtered_users(subject: str, session: Session):
-    queue = session.query(Staging).all()
-    filtered = []
-    for user in queue:
-        if user.subject == subject:
-            filtered.append(user)
-    return filtered
-
-
-def get_random_user(users: list):
-    random_id = random.randint(0, len(users))
-    try:
-        random_user = users[random_id]
-        return random_user
-    except IndexError:
-        return False
-
-
-def get_random_task(tasks: list):
-    random_task_index = random.randint(0, len(tasks) - 1)
-    try:
-        random_task = tasks[random_task_index]
-        return random_task
-    except IndexError:
-        return False
-
-
-def search_opponent(users: list, user: User):
-    flag = False
-    random_user = None
-    while not flag:
-        random_user = get_random_user(users=users)
-        if random_user and random_user.user_id != user.id:
-            flag = True
-    return random_user
-
-
-def database_users_adding(user: User, opponent: User, session: Session):
-    user_opponent = Game(opponent_id=opponent.id)
-    opponent_opponent = Game(opponent_id=user.id)
-    session.add(user_opponent)
-    user.game.append(user_opponent)
-    session.add(opponent_opponent)
-    opponent.game.append(opponent_opponent)
-    session.commit()
-    delete_from_staging(user=user, session=session)
-    delete_from_staging(user=opponent, session=session)
-    session.commit()
-
-
-def database_task_adding(task: Task, user_id: int,
-                         opponent_id: int, session: Session):
-    user_game = session.query(Game).filter_by(user_id=user_id, opponent_id=opponent_id).first()
-    opponent_game = session.query(Game).filter_by(user_id=opponent_id, opponent_id=user_id).first()
-    task.games.append(user_game)
-    task.games.append(opponent_game)
-    session.commit()
 
 
 def add_to_game(user: User, session: Session):
