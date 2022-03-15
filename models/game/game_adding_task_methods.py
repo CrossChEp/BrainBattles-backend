c9d95379dce3c4d6e700a98ed5a53a25ef0c1f98
Anderzhanov Oleg <anderzanovoleg@gmail.com>
2022-03-15 19:52:37 +0300

    rank tasks filtration has done

--- models/game/game_adding_task_methods.py
+++ models/game/game_adding_task_methods.py
@@ -1,12 +1,15 @@
 from sqlalchemy.orm import Session
 
+from configs import ranks
+from models.game.game_adding_rank_methods import add_ranks_list
 from store import User, Task
 
 
 def filter_task_by_rank(user: User, subject: str, session: Session):
-    tasks = session.query(Task).filter_by(subject=subject).all()
+    db_tasks = session.query(Task).filter_by(subject=subject).all()
+    tasks_list = add_ranks_list(ranks=ranks)
     filtered_tasks = []
-    for task in tasks:
-        if task.rank <= user.rank:
+    for task in db_tasks:
+        if tasks_list.index(task.rank) <= tasks_list.index(user.rank):
             filtered_tasks.append(task)
     return filtered_tasks
