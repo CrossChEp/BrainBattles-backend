1a19f746ce9e5a3962aeffb732bfb940d5f4b8b9
Andy <anderzanovoleg@gmail.com>
2022-05-11 22:37:20 +0300

    searching task system was remade

--- models/game/game_adding_task_methods.py
+++ models/game/game_adding_task_methods.py
@@ -17,6 +17,6 @@ def filter_task_by_rank(user: User, subject: str, session: Session):
     tasks_list = add_ranks_list(ranks=ranks)
     filtered_tasks = []
     for task in db_tasks:
-        if tasks_list.index(task.rank) <= tasks_list.index(user.rank):
+        if tasks_list.index(task.rank) <= tasks_list.index(user.rank) and not task.is_moderated:
             filtered_tasks.append(task)
     return filtered_tasks
